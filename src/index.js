934f66a09f43d2ed5f3d1a7a56902bcb867de7d2
Sarah Rose Battles <srosebattles@gmail.com>
2016-11-20 20:15:08 -0600

    yay for inline block

--- src/index.js
+++ src/index.js
@@ -8,19 +8,37 @@ $(document).ready(function(){
 $.ajax({
    url: "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Q&limit=10&apikey=cfe92312902acf787c24d7d60c0f917c"
  }).then(function(response) {
+
 var group = $('<div class="group"></div>')
 
 $.each(response.data.results, function(i, char){
   console.log(this)
-  var charImage =$('<img />').attr("src", this.thumbnail.path + "/standard_large." + this.thumbnail.extension)
+  var charImage =$('<img />').attr("src", this.thumbnail.path + "/standard_xlarge." + this.thumbnail.extension)
   var imgDiv = $('<div class="imgDiv"></div>')
-  console.log(charImage)
+  var textDiv = $('<div class="imgDiv"></div>')
   imgDiv.append(charImage)
   imgDiv.append("<br>" + this.name + "<br>")
   group.append(imgDiv)
   // if ("class=charID" === checked){
   //   $("#characterstuff").append("Character ID number: " + this.id + "<br>");
   // }
+  textDiv.append("Character ID number: " + this.id + "<br>")
+  
+  if (this.description === "" || this.description === null){
+    textDiv.append("There is no character description available right now. Our bad!")
+  } else {
+    textDiv.append(this.description)
+  }
+
+  group.append(textDiv)
+
+
+
+  //  $.each(response.data.results.comics.items), function(i, com){
+  // console.log(this.name)
+  // //   console.log(this)
+  // //   console.log(comics.items.name)
+  //  }
 
   $("#characterstuff").append(group);
   group.append("<hr/>");

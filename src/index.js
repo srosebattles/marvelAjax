99e7d0ab7f5259d50a39d6607467ebdb48bbeaa5
Sarah Rose Battles <srosebattles@gmail.com>
2016-11-21 14:35:06 -0600

    duringMonday

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
     // console.log('DOM IS READY');
 
-    // $("form").on("submit", function(e){
-    //   $("input:checkbox:checked").val())
-    // })
+    $("form").on("submit", function(e){
+      e.preventDefault();
+    $.each($("input[type='checkbox']:checked"), function(i, v){
+      var whatChecked = $(v).val()
+      return whatChecked
+    })
+
+    })
+
+    // function showStuff(){}
 
 $.ajax({
    url: "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Q&limit=10&apikey=cfe92312902acf787c24d7d60c0f917c"
@@ -11,45 +18,48 @@ $.ajax({
 
 var group = $('<div class="group"></div>')
 
+
 $.each(response.data.results, function(i, char){
-  console.log(this)
+  var subgroup = $('<div class="subgroup"></div>')
   var charImage =$('<img />').attr("src", this.thumbnail.path + "/standard_xlarge." + this.thumbnail.extension)
   var imgDiv = $('<div class="imgDiv"></div>')
   var textDiv = $('<div class="textDiv"></div>')
   var comicsDiv = $('<div class="comicsDiv"></div>')
   imgDiv.append(charImage)
   imgDiv.append("<br>" + this.name + "<br>")
-  group.append(imgDiv)
-  // if ("class=charID" === checked){
-  //   $("#characterstuff").append("Character ID number: " + this.id + "<br>");
-  // }
-  textDiv.append("Character ID number: " + this.id + "<br>")
+  subgroup.append(imgDiv)
+
+   if (whatChecked === charID){
+textDiv.append("Character ID number: " + this.id + "<br>");
+   }
+
+  // textDiv.append("Character ID number: " + this.id + "<br>")
 
   if (this.description === "" || this.description === null){
-    textDiv.append("There is no character description available right now. Our bad!")
+    textDiv.append("There is no character description available right now. Our bad!" + "<br>")
   } else {
     textDiv.append(this.description)
   }
 
-  group.append(textDiv)
+  subgroup.append(textDiv)
 
   imgDiv.append(this.comics.items.name)
-  console.log(this.comics.items)
-  group.append(comicsDiv)
+  subgroup.append(comicsDiv)
 
 
+      if (this.comics.items.length > 0){
+        $.each(this.comics.items, function(i, com){
+         comicsDiv.append(com.name)
 
-  //   $.each(this.comics), function(i, com){
-  //     if (this.comics < 1) {
-  //       return ""
-  //     }
-  // //  console.log(this.name)
-  // // // //   console.log(this)
-  //    console.log(comics.items.name)
-  //    }
+       })
+     } else {
+       comicsDiv.append("Sorry, we can't find a record of this character's comics")
+     }
 
+       subgroup.append("<hr/>");
+       group.append(subgroup)
   $("#characterstuff").append(group);
-  group.append("<hr/>");
+
 
 }
 )
